da7240c6d0c158a3b78bb5d11da7a91574ace489
beta3000 <atlas.aresco@gmail.com>
2016-05-18 17:31:59 -0500

    Renombrado Login

--- src/com/mycompany/ws/action/loginUsuarioAction.java
+++ src/com/mycompany/ws/action/loginUsuarioAction.java
@@ -9,7 +9,7 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import java.util.Map;
 
-public class loginUsuarioAction extends ActionSupport implements SessionAware {
+public class LoginUsuarioAction extends ActionSupport implements SessionAware {
 
     private UsuarioBean usuarioBean;
     private String email, password;
